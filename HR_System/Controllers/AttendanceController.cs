7742e89b9c5dab8be0420cef75f56d49f95da6a8
Mostaf Ahmed <80547961+Mostafa-Ahmed1@users.noreply.github.com>
2021-12-04 04:36:13 +0200

    Attaendance Controller

---
+++ HR_System/Controllers/AttendanceController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HR_System.Models;
+
+
+
+namespace HR_System.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        HrSysContext db;
+        public AttendanceController(HrSysContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
