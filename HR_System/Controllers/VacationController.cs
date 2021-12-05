8378bf35b7fbf74e9e51a25f3ce0fa5563905a1b
mostafa ali <mostafa.ali.elnemr92@gmail.com>
2021-12-05 21:49:02 +0300

    First

---
+++ HR_System/Controllers/VacationController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HR_System.Models;
+
+namespace HR_System.Controllers
+{
+    public class VacationController : Controller
+    {
+        HrSysContext db;
+        public VacationController(HrSysContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Vacation v)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Vacations.Add(v);
+                db.SaveChanges();
+                return RedirectToAction("display");
+            }
+            else
+            {
+                return View(v);
+
+            }
+
+        }
+        public IActionResult display()
+        {
+            return View(db.Vacations.ToList());
+        }
+
+    }
+}
