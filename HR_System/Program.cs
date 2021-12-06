e72dde56cccb9e1b4dcc5e80fdb88e1e2d3d5d0d
mohamed shukry <mohamed.shukry412@gmail.com>
2021-12-07 00:10:54 +0200

    sessions

--- HR_System/Program.cs
+++ HR_System/Program.cs
@@ -38,6 +38,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=operation}/{action=login}/{id?}");
 
 app.Run();
