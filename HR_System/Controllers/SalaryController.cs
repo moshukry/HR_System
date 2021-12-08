28554086dde29d748d9e2365a059f0c78bd73d88
Mohamed Nabawey <92363499+nabawey@users.noreply.github.com>
2021-12-08 04:02:50 +0200

    Nabawey (#25)
    * Dashboard (#22)
    
    * change new Layout
    
    * Attaendance Controller
    
    * Return
    
    * Index Finish
    
    * Nabawey (#9)
    
    * User CRUD
    GROUP And Pages Check Boxes
    4/12
    
    * User & Employee CRUD
    
    * Nabawey (#10)
    
    * User CRUD
    
    * User CRUD
    GROUP And Pages Check Boxes
    
    * User CRUD 4/12
    
    * USER & EMP CRUD 4/12
    
    * fail trail of departure time validation
    
    * Link
    
    * _Layout Edit
    
    * Create
    
    * Create with btns
    
    * Finish without UX
    
    * Update Index
    
    * First
    
    * Finished
    
    * Create
    
    * sessions
    
    * paging trisl
    
    * Index
    
    * Function
    
    * login
    
    * emp 6/12
    
    * q
    
    * sessions
    
    * sessions 2
    
    * poluy
    
    * Update HR_System.csproj
    
    * logout
    
    * Dashboard Controller
    
    Co-authored-by: Mostaf Ahmed <80547961+Mostafa-Ahmed1@users.noreply.github.com>
    Co-authored-by: mohamed shukry <91802935+moshukry@users.noreply.github.com>
    Co-authored-by: mohamed shukry <mohamed.shukry412@gmail.com>
    Co-authored-by: mostafa ali <mostafa.ali.elnemr92@gmail.com>
    Co-authored-by: MostafaAli1292 <48778214+MostafaAli1292@users.noreply.github.com>
    Co-authored-by: Ghadeer-Gamal <52006758+Ghadeer-Gamal@users.noreply.github.com>
    
    * Dynamic Layout
    
    * Salary V1
    
    Co-authored-by: Mostaf Ahmed <80547961+Mostafa-Ahmed1@users.noreply.github.com>
    Co-authored-by: mohamed shukry <91802935+moshukry@users.noreply.github.com>
    Co-authored-by: mohamed shukry <mohamed.shukry412@gmail.com>
    Co-authored-by: mostafa ali <mostafa.ali.elnemr92@gmail.com>
    Co-authored-by: MostafaAli1292 <48778214+MostafaAli1292@users.noreply.github.com>
    Co-authored-by: Ghadeer-Gamal <52006758+Ghadeer-Gamal@users.noreply.github.com>

---
+++ HR_System/Controllers/SalaryController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HR_System.Models;
+using HR_System.ViewModels;
+
+namespace HR_System.Controllers;
+public class SalaryController : Controller
+{
+        HrSysContext db;
+
+        public SalaryController(HrSysContext db)
+        {
+               this.db = db;
+        }
+
+    public IActionResult Index()
+    { 
+
+        List<SalaryVM> salary = new List<SalaryVM>();
+
+        List<Employee> employees = db.Employees.ToList();
+
+        List<AttDep> attendence = db.Att_dep.ToList();
+
+
+        // Attendence Days using Explicit Loading 
+        //var x = db.Employees.Single( n => n.EmpId == 1);
+        //var y = db.Entry(x).Collection(n => n.AttDeps).Query().Count();
+        
+        foreach (var emp in employees)
+        {
+            salary.Add(new SalaryVM()
+            {
+                fixedSalary = emp.FixedSalary, 
+                employeeName = emp.EmpName,
+                departmentName = emp.Dept.DeptName ,
+                attendenceDays = db.Entry(db.Employees.Single(n => n.EmpId == emp.EmpId)).Collection(n => n.AttDeps).Query().Where(n=> n.Date.Month==11 && n.Date.Year == 2011).Count() ,
+               // abscenseDays =  db.Entry(db.Employees.Single(n =>n.EmpId == emp.EmpId)).Collection(n => n.AttDeps).Query().Where(n=> n.Date.Month == 11 && n.Date.Year == 2011).Count() 
+            });
+
+
+        }
+
+
+        return View(salary);
+    }
+}
---
+++ HR_System/ViewModels/SalaryVM.cs
@@ -0,0 +1,17 @@
+﻿using HR_System.Models;
+namespace HR_System.ViewModels;
+public class SalaryVM
+{
+    public string employeeName { get; set; }
+    public string departmentName { get; set; }
+    public int fixedSalary { get; set; }
+   
+    public int attendenceDays {  get; set; }
+
+    public int abscenseDays {  get; set; }
+
+
+
+
+
+}
