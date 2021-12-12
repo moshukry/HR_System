b6219317822c25677fdd0f2c2b8f4600521ab8bf
mohamed shukry <mohamed.shukry412@gmail.com>
2021-12-12 18:40:43 +0200

    dashboard

--- HR_System/Controllers/DashboardController.cs
+++ HR_System/Controllers/DashboardController.cs
@@ -16,36 +16,12 @@ namespace HR_System.Controllers
         public DashboardController(HrSysContext context)
         {
             _context = context;
-          }
-            userORAdmin.admin = db.Admins.Find(int.Parse(admin_id));
-            return View(userORAdmin);
         }
-        else if (user_id != null)
-        {
-            var gId = HttpContext.Session.GetString("groupId");
-            if (gId != null)
-            {
-#pragma warning disable CS8604 // Possible null reference argument.
-                List<Crud> Rules = db.CRUDs.Where(n => n.GroupId == int.Parse(gId)).ToList();
-#pragma warning restore CS8604 // Possible null reference argument.
-                ViewBag.PagesRules = Rules;
-               
-            }
-            userORAdmin.user = db.Users.Find(int.Parse(user_id));
-            return View(userORAdmin);
-
-
-            
-      
-
-
         // GET: Dashboard
         public async Task<IActionResult> Index()
         {
             var hrSysContext = _context.Att_dep.Include(a => a.Emp);
             return View(await hrSysContext.ToListAsync());
         }
-
-      
     }
 }
